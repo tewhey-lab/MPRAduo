0b9e4ce64ff239b414bf28eb43f640a3c9384e0a
deweyhannah <57405854+deweyhannah@users.noreply.github.com>
2019-11-08 13:56:24 -0500

    Updated
    Updated to include more accurate statistics

--- library_separation_FLASH2_input.py
+++ library_separation_FLASH2_input.py
@@ -33,9 +33,23 @@ libP_dict = dict(zip(BC_P[0], BC_P[1]))
 
 i=0
 count_single_no_match = 0
+count_single_A_no_match = 0
+count_single_P_no_match = 0
 count_duo_no_match = 0
+count_duo_AiP_no_oligo = 0
+count_duo_PiA_no_oligo = 0
+count_duo_AiP_no_A = 0
+count_duo_AiP_no_P = 0
+count_duo_PiA_no_P = 0
+count_duo_PiA_no_A = 0
 count_single = 0
+count_single_A = 0
+count_single_P = 0
 count_duo = 0
+count_duo_AiP = 0
+count_duo_AiP_both = 0
+count_duo_PiA = 0
+count_duo_PiA_both = 0
 no_oligo_match = 0
 # Open write files and begin parsing the fastq file
 with open ("/projects/tewhey-lab/mourik/MPRAduo_Hannah/output/%s/matched_all.txt" % out_dir, "w") as matched_all_oligo:
@@ -70,20 +84,24 @@ with open ("/projects/tewhey-lab/mourik/MPRAduo_Hannah/output/%s/matched_all.txt
                             if link_P in seq_only[fill_start+19:fill_start+28]:
                                 lib_P_index = seq_only.index(link_P)
                             if lib_A_index > 0 and lib_P_index == 0:
+                                count_single_A += 1
                                 g = seq_only[lib_A_index+6:lib_A_index+26]
-                            if g in libA_dict:
-                                matched_all_oligo.write("%s\t%s\n" % (g,libA_dict[g]))
-                            if g not in libA_dict:
-                                matched_all_oligo.write("%s\n" % g)
-                                no_oligo_match += 1
-                                SeqIO.write(record, no_match_oligo, "fastq")
+                                if g in libA_dict:
+                                    matched_all_oligo.write("%s\t%s\n" % (g,libA_dict[g]))
+                                if g not in libA_dict:
+                                    matched_all_oligo.write("%s\n" % g)
+                                    no_oligo_match += 1
+                                    count_single_A_no_match += 1
+                                    SeqIO.write(record, no_match_oligo, "fastq")
                             if lib_P_index > 0 and lib_A_index == 0:
+                                count_single_P += 1
                                 k = seq_only[lib_P_index+6:lib_P_index+16]
                                 if k in libP_dict:
                                     matched_all_oligo.write("%s\t%s\n" % (k, libP_dict[k]))
                                 if k not in libP_dict:
                                     matched_all_oligo.write("%s\n" % k)
                                     no_oligo_match += 1
+                                    count_single_P_no_match += 1
                                     SeqIO.write(record, no_match_oligo, "fastq")
                             if lib_A_index == 0 and lib_P_index == 0:
                                 count_single_no_match += 1
@@ -105,30 +123,40 @@ with open ("/projects/tewhey-lab/mourik/MPRAduo_Hannah/output/%s/matched_all.txt
                                 seq_only_PiA = seq_only[lib_P_index+55:]
                                 lib_PiA_index = seq_only_PiA.index(link_A)
                             if lib_A_index > 0 and lib_AiP_index > 0:
+                                count_duo_AiP += 1
                                 g = seq_only[lib_A_index+6:lib_A_index+26]
                                 k = seq_only_AiP[lib_AiP_index+6:lib_AiP_index+16]
                                 if g in libA_dict and k in libP_dict:
+                                    count_duo_AiP_both += 1
                                     matched_all_oligo.write("%s^%s\t%s^%s\n" % (g,k,libA_dict[g],libP_dict[k]))
                                 if g in libA_dict and k not in libP_dict:
+                                    count_duo_AiP_no_P += 1
                                     matched_all_oligo.write("%s^%s\t%s^\n" % (g,k,libA_dict[g]))
                                 if g not in libA_dict and k in libP_dict:
                                     matched_all_oligo.write("%s^%s\t^%s\n" % (g,k,libP_dict[k]))
+                                    count_duo_AiP_no_A += 1
                                 if g not in libA_dict and k not in libP_dict:
                                     matched_all_oligo.write("%s^%s\t^\n" % (g,k))
                                     no_oligo_match += 1
+                                    count_duo_AiP_no_oligo += 1
                                     SeqIO.write(record, no_match_oligo, "fastq")
                             if lib_P_index > 0 and lib_PiA_index > 0:
+                                count_duo_PiA += 1
                                 g = seq_only[lib_P_index+6:lib_P_index+26]
                                 k = seq_only_PiA[lib_PiA_index+6:lib_PiA_index+16]
                                 if g in libP_dict and k in libA_dict:
+                                    count_duo_PiA_both += 1
                                     matched_all_oligo.write("%s^%s\t%s^%s\n" % (g,k,libP_dict[g],libA_dict[k]))
                                 if g in libP_dict and k not in libA_dict:
+                                    count_duo_PiA_no_A += 1
                                     matched_all_oligo.write("%s^%s\t%s^\n" % (g,k,libP_dict[g]))
                                 if g not in libP_dict and k in libA_dict:
+                                    count_duo_PiA_no_P += 1
                                     matched_all_oligo.write("%s^%s\t^%s\n" % (g,k,libA_dict[k]))
                                 if g not in libP_dict and k not in libA_dict:
                                     matched_all_oligo.write("%s^%s\t^\n" % (g,k))
                                     no_oligo_match += 1
+                                    count_duo_PiA_no_oligo += 1
                                     SeqIO.write(record, no_match_oligo, "fastq")
                             if lib_A_index == 0 and lib_AiP_index > 0 or lib_A_index > 0 and lib_AiP_index == 0:
                                 count_duo_no_match += 1
@@ -151,4 +179,8 @@ with open ("/projects/tewhey-lab/mourik/MPRAduo_Hannah/output/%s/statistics.txt"
     run_stats.write("Percent Matched to Library: %f \n" % float(1-((count_duo_no_match+count_single_no_match)/i)))
     run_stats.write("Percent Duo Matched: %f \n" % float(1-(count_duo_no_match/count_duo)))
     run_stats.write("Percent Single Matched: %f \n" % float(1-(count_single_no_match/count_single)))
-    run_stats.write("Percent Matched with No Oligos: %f" % float(no_oligo_match/((count_duo-count_duo_no_match)+(count_single-count_single_no_match))))
+    run_stats.write("Percent Matched with No Oligos: %f\n" % float(no_oligo_match/((count_duo-count_duo_no_match)+(count_single-count_single_no_match))))
+    run_stats.write("A into P: \n \tPercent Both Oligos Matched: %f\n \tPercent No Oligos Matched: %f\n \tPercent only A oligo matched: %f\n \tPercent only P oligo matched: %f\n" % (float(count_duo_AiP_both/count_duo_AiP), float(count_duo_AiP_no_oligo/count_duo_AiP), float(count_duo_AiP_no_P/count_duo_AiP), float(count_duo_AiP_no_A/count_duo_AiP)))
+    run_stats.write("P into A: \n \tPercent Both Oligos Matched: %f\n \tPercent No Oligos Matched: %f\n \tPercent only A oligo matched: %f\n \tPercent only P oligo matched: %f\n" % (float(count_duo_PiA_both/count_duo_PiA), float(count_duo_PiA_no_oligo/count_duo_PiA), float(count_duo_PiA_no_P/count_duo_PiA), float(count_duo_PiA_no_A/count_duo_PiA)))
+    run_stats.write("Single A: \n \tPercent Oligos Matched: %f\n \tPercent Oligos Not Matched: %f\n" % (float(1-(count_single_A_no_match/count_single_A)), float(count_single_A_no_match/count_single_A)))
+    run_stats.write("Single P: \n \tPercent Oligos Matched: %f\n \tPercent Oligos Not Matched: %f\n" % (float(1-(count_single_P_no_match/count_single_P)), float(count_single_P_no_match/count_single_P)))
